c5c2a33880b4cb24be5b6c9df879fd806053a2ae
Regular Dude <rglr_dude@pm.me>
2025-01-20 16:29:21 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LectionarySearch from './LectionarySearch';
+
+function App() {
+  return (
+    <div className="p-4">
+      <LectionarySearch />
+    </div>
+  );
+}
+
+export default App;
